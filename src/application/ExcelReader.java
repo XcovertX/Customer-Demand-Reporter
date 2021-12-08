49664d8d232617aeaa1882a2c99e3fdb42220612
James Covert <jcovert2@asu.edu>
2021-12-08 07:27:29 -0800

    commiting changes before rebuilding gradle.build file

---
+++ src/application/ExcelReader.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+//import org.apache.poi.ss.usermodel.Cell;
+//import org.apache.poi.ss.usermodel.CellStyle;
+//import org.apache.poi.ss.usermodel.Font;
+//import org.apache.poi.ss.usermodel.Row;
+//import org.apache.poi.ss.usermodel.Sheet;
+//import org.apache.poi.ss.usermodel.Workbook;
+
+public class ExcelReader {
+	
+	
+	public void read(String fileLocation) {
+//		FileInputStream file = new FileInputStream(new File(fileLocation));
+//		Workbook workbook = new XSSFWorkbook(file);
+	}
+
+}
